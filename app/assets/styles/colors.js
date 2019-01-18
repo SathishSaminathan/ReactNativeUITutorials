7422c21797c95a3c82c440ea9788978ea18d3150
SathishSaminathan <Sathishcse1975@gmail.com>
2019-01-18 11:18:14 +0530

    initial_commit

---
+++ app/assets/styles/colors.js
@@ -0,0 +1,16 @@
+// export const COLOR_PRIMARY = '#58C9B9'; Amazon
+// export const COLOR_PRIMARY = '#008388';
+// export const COLOR_SECONDARY = '#111';
+// export const SUCCESS_COLOR ="#5cb85c";
+// export const DANGER_COLOR = "#d9534f";
+// export const PRIMARY_COLOR = "#2c3e50"
+
+export default {
+    COLOR_PRIMARY : '#008388',
+    COLOR_PRIMARY_PINK : "#fc6c8f",
+    COLOR_SECONDARY : "#111",
+    SUCCESS_COLOR :"#5cb85c",
+    DANGER_COLOR : "#d9534f",
+    PRIMARY_COLOR : "#2c3e50",
+    SITE_GRAY1 : "#54575e"
+}
---
+++ app/assets/styles/common.js
@@ -0,0 +1,44 @@
+// export const COLOR_PRIMARY = '#58C9B9';
+
+
+export const COLOR_PRIMARY = '#ff1e8ff5';//pink
+// export const COLOR_PRIMARY = '#008388';//blue
+export const COLOR_SECONDARY = '#58C9B9';
+export const COLOR_PRIMARY_PINK = "#fc6c8f";
+// export const PINK_TRANSPARENT= "#ea87b96b";
+export const BUTTON_PINK= "#ff1e8ff5";
+export const SUCCESS_COLOR ="#5cb85c";
+export const DANGER_COLOR = "#d9534f";
+export const PRIMARY_COLOR = "#2c3e50";
+export const SITE_GRAY1 = "#54575e";
+export const FONT_NORMAL = 'OpenSans-Regular';
+export const FONT_BOLD = 'OpenSans-Bold';
+export const BORDER_RADIUS = 5;
+export const XXLARGE = 60;
+export const XLARGE = 40;
+export const LARGE = 30;
+export const MEDIUM = 24;
+export const SMALL = 12;
+
+export default { 
+    // COLOR_PRIMARY : '#ff1e8ff5',// dark-pink
+    COLOR_PRIMARY : '#ff265bba',//light-pink
+    COLOR_PRIMARY_BLUE : '#008388',//blue
+    COLOR_SECONDARY : '#58C9B9',
+    COLOR_PRIMARY_PINK : "#fc6c8f",
+    COLOR_PRIMARY_PINK_BACKGROUND : "#b11339c2",
+    PINK_TRANSPARENT: "#ea87b96b",
+    BUTTON_PINK:"#ff1e8ff5",
+    SUCCESS_COLOR :"#5cb85c",
+    DANGER_COLOR : "#d9534f",
+    PRIMARY_COLOR : "#2c3e50",
+    SITE_GRAY1 : "#54575e",
+    FONT_NORMAL : 'OpenSans-Regular',
+    FONT_BOLD : 'OpenSans-Bold',
+    BORDER_RADIUS : 5,
+    XXLARGE : 60,
+    XLARGE : 40,
+    LARGE : 30,
+    MEDIUM : 24,
+    SMALL : 15,
+}
